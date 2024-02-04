2a4a745369ae966ce082edad1b338aa89643a1d4
Severgin Andrey <severgin.azaza@yandex.ru>
2024-02-04 20:56:50 +0800

    [upd] micro fix -> rewriting string representations & add meta to wells_characteristics

--- practice/chars_catalog/models.py
+++ practice/chars_catalog/models.py
@@ -6,41 +6,40 @@ class CharacteristicType(models.Model):
 
     class Meta:
         db_table = 'characteristic_types'
-        verbose_name = 'Типы характеристик'
-        verbose_name_plural = 'Типы характеристик'
+        verbose_name = 'Виды характеристик'
+        verbose_name_plural = 'Виды характеристик'
 
     name = models.CharField(max_length=250, unique=True, verbose_name='Название')
     max_value_x = models.PositiveIntegerField(verbose_name='Максимальное значение по оси x')
     max_value_y = models.PositiveIntegerField(verbose_name='Максимальное значение по оси y')
-    json_key_name = models.CharField(max_length=250, verbose_name='Название ключа в поле json')
-    json_value_name = models.CharField(max_length=250, verbose_name='Название значения в поле json')
+    json_key_name = models.CharField(max_length=250, verbose_name='Ключ в json')
+    json_value_name = models.CharField(max_length=250, verbose_name='Значение в json')
 
     def __str__(self):
-        return f"{self.name} {self.max_value_x} {self.max_value_y}"
+        return f"{self.name}"
     
 class Characteristic(models.Model):
     """Характеристика"""
 
     class Meta:
         db_table = 'characteristics'
-        verbose_name = 'Описание характеристики'
-        verbose_name_plural = 'Описание характеристики'
+        verbose_name = 'Характеристики'
+        verbose_name_plural = 'Характеристики'
 
     value = JSONField(verbose_name="Значение")
     characteristic_type = models.ForeignKey(CharacteristicType, on_delete=models.CASCADE, verbose_name="Вид характеристики")
     name = models.CharField(max_length=250, verbose_name='Название')
 
     def __str__(self):
-        return f"{self.characteristic_type} {self.name}"
+        return f"{self.name}"
     
 class Well(models.Model):
     """Скважина"""
 
     class Meta:
-        abstract = True
         db_table = 'wells'
-        verbose_name = 'Описание скважины'
-        verbose_name_plural = 'Описание скважины'
+        verbose_name = 'Скважины'
+        verbose_name_plural = 'Скважины'
 
     name = models.CharField(max_length=250, verbose_name='Название')
     
@@ -49,6 +48,11 @@ class Well(models.Model):
     
 class WellCharacteristicBinding(models.Model):
     """Привязка характеристик к скважине"""
+
+    class Meta:
+        db_table = 'wells_characteristics'
+        verbose_name = 'Характеристики скважин'
+        verbose_name_plural = 'Характеристики скважин'
 
     well = models.ForeignKey(Well, on_delete=models.CASCADE)  # Используем абстрактную модель Well
     characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
