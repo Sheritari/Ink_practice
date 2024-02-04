a5573817b626dbb1dac23c8f77950efdc40f9256
Severgin Andrey <severgin.azaza@yandex.ru>
2024-02-04 20:57:51 +0800

    [upd] Step 3 -> adding models to the admin panel

--- practice/chars_catalog/admin.py
+++ practice/chars_catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import CharacteristicType, Characteristic, WellCharacteristicBinding, Well
 
-# Register your models here.
+# Регистрация модели "Справочник типов характеристик" в админке
+@admin.register(CharacteristicType)
+class CharacteristicTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'max_value_x', 'max_value_y']
+
+# Регистрация модели "Характеристика" в админке
+@admin.register(Characteristic)
+class CharacteristicAdmin(admin.ModelAdmin):
+    list_display = ['name', 'characteristic_type']
+
+# Регистрация модели "Привязка характеристик к скважине" в админке
+@admin.register(WellCharacteristicBinding)
+class WellCharacteristicBindingAdmin(admin.ModelAdmin):
+    list_display = ['well', 'characteristic']
+
+# Регистрация модели "Скважина" в админке
+@admin.register(Well)
+class WellAdmin(admin.ModelAdmin):
+    list_display = ['name']
