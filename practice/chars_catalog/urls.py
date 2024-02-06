ee3e2374de2a326816bb2d8b348f2ae1781fb1b3
Severgin Andrey <severgin.azaza@yandex.ru>
2024-02-06 13:45:00 +0800

    [upd] micro fix -> transfer routes to the app dir

--- practice/chars_catalog/urls.py
+++ practice/chars_catalog/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView
+from django.urls import path, include
+from .views import RegisterView, LoginView, UserView, LogoutView, CharacteristicTypeViewSet, CharacteristicViewSet, WellViewSet, WellCharacteristicViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register(r'chartype', CharacteristicTypeViewSet)
+router.register(r'char', CharacteristicViewSet)
+router.register(r'well', WellViewSet)
+router.register(r'wellchar', WellCharacteristicViewSet)
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('login', LoginView.as_view()),
     path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view())
+    path('logout', LogoutView.as_view()),
+    path('', include(router.urls)),
 ]
--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from chars_catalog.views import CharacteristicTypeViewSet, CharacteristicViewSet, WellViewSet, WellCharacteristicViewSet, RegisterView
-
-router = routers.DefaultRouter()
-
-router.register(r'chartype', CharacteristicTypeViewSet)
-router.register(r'char', CharacteristicViewSet)
-router.register(r'well', WellViewSet)
-router.register(r'wellchar', WellCharacteristicViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
     path('api/v1/', include('chars_catalog.urls'))
 ]
